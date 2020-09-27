f8a428c7a891618bba76010f0ee72b8ab8e6a070
Cudiph <dwiaceromo@gmail.com>
2020-09-28 01:25:05 +0700

    remodel ping.js

--- src/Modules/events/ping.js
+++ src/Modules/events/ping.js
@@ -1,21 +1,36 @@
 const Discord = require('discord.js');
+const { Command } = require('discord.js-commando');
 
-module.exports = {
-  name: "ping",
-  cooldown: 7,
-  description: "Another one",
-  execute(message, args) {
-    message.channel.send("Pinging...").then(msg => {
-      // The math thingy to calculate the user's ping
-      let ping = msg.createdTimestamp - message.createdTimestamp;
-      // Basic embed
-      const embed = new Discord.MessageEmbed()
-        .setDescription(`Your ping is **${ping}**ms`)
-        .setColor("#11ff00")
-
-      // Then It Edits the message with the ping variable embed that you created
-      msg.delete();
-      message.channel.send(embed)
+module.exports = class MeowCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'ping',
+      aliases: ['p'],
+      group: 'events',
+      memberName: 'ping',
+      description: 'Checks the bot\'s latency to the Discord server.',
+      throttling: {
+        usages: 5,
+        duration: 10,
+      },
     });
   }
-}
+
+  async run(message) {
+    message.channel.send("Pinging...").then(pingMsg => {
+      // Basic embed
+      const embed = new Discord.MessageEmbed()
+        .setColor("#11ff00")
+        .setDescription(`Pong! 🏓`)
+        .addFields(
+          { name: `RTT Ping`, value: `${(pingMsg.editedTimestamp || pingMsg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms.`, inline: true},
+          { name: `HB Ping`, value: `${Math.round(this.client.ws.ping)}ms.`, inline: true},
+        )
+        .setFooter(`Region: ${message.guild.region}`, message.guild.iconURL())
+
+      // Then It Edits the message with the ping variable embed that you created
+      pingMsg.delete();
+      message.channel.send(embed);
+    });
+  }
+};
