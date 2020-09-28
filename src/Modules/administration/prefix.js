f6fc1d450d81b29721cdbaa2b5a4135ac9f84d63
Cudiph <dwiaceromo@gmail.com>
2020-09-29 03:06:42 +0700

    remodel prefix.js

--- src/Modules/administration/prefix.js
+++ src/Modules/administration/prefix.js
@@ -1,60 +1,103 @@
 const Discord = require('discord.js');
+const { Command } = require('discord.js-commando');
 const { crud } = require('../../library/Database/crud.js');
 const { guildSettingsSchema } = require('../../library/Database/schema.js');
+const { oneLine, stripIndents } = require('common-tags');
+const { client } = require('../../bot.js');
 
-module.exports = {
-  name: 'prefix',
-  description: 'set default prefix for the server',
-  guildOnly: true,
-  args: true,
-  async execute(message, args) {
-    // define old prefix and mongo url
-    let oldPrefix = '';
-    let url = process.env.MONGO_URL;
-    if (!args.length) {
-      // if no arg defines then send current guild prefix
-      let db = await new crud(url).connect();
-      try {
-        const result = await guildSettingsSchema.findById(message.guild.id);
-        message.channel.send(`This guild current prefix is : "**${result.prefix}**"`)
-      } catch (err) {
-        logger.log('error', err);
-      } finally {
-        db.connection.close();
-      }
-      return;
-    }
-    if (!message.member.hasPermission("ADMINISTRATOR")) {
-      return message.reply(`You do not have permission to do that`);
-    }
-    // set new prefix for guild
-    let db = await new crud(url).connect();
-    try {
-      let result = await guildSettingsSchema.findOneAndUpdate({
-        _id: message.guild.id
-      }, {
-        _id: message.guild.id,
-        prefix: args[0]
-      },
-        {
-          upsert: true
-        });
-      oldPrefix = result.prefix;
-    } catch (err) {
-      logger.log('error', err);
-      return message.channel.send(`Can't update the prefix.`);
-    } finally {
-      db.connection.close();
-    }
-    // define new variable in cache
-    prefixCache[message.guild.id] = args[0];
-
-    const embed = new Discord.MessageEmbed()
-      .setColor('#ff548e')
-      .setDescription(`This guild prefix has been updated`)
-      .addField('From', `**${oldPrefix}**`, true)
-      .addField('To', `**${args[0]}**`, true);
-    // send embed
-    message.channel.send(embed);
+const writePrefix = async (newPrefix, msg) => {
+  // set new prefix for guild
+  let url = process.env.MONGO_URL;
+  let db = await new crud(url).connect();
+  try {
+    let result = await guildSettingsSchema.findOneAndUpdate({
+      _id: msg.guild.id
+    }, {
+      _id: msg.guild.id,
+      prefix: newPrefix,
+    }, {
+      upsert: true
+    });
+    if (result) return result.prefix; else return client.commandPrefix;
+  } catch (err) {
+    logger.log('error', err);
+    return msg.channel.send(`Can't update the prefix.`);
+  } finally {
+    db.connection.close();
   }
 }
+
+module.exports = class PrefixCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'prefix',
+      group: 'administration',
+      memberName: 'prefix',
+      description: 'Shows or sets the command prefix.',
+      format: '[prefix/"default"/"none"]',
+      details: oneLine`
+				If no prefix is provided, the current prefix will be shown.
+				If the prefix is "default", the prefix will be reset to the bot's default prefix.
+				If the prefix is "none", the prefix will be removed entirely, only allowing mentions to run commands.
+				Only administrators may change the prefix.
+			`,
+      examples: ['prefix', 'prefix -', 'prefix omg!', 'prefix default', 'prefix none'],
+
+      args: [
+        {
+          key: 'prefix',
+          prompt: 'What would you like to set the bot\'s prefix to?',
+          type: 'string',
+          max: 15,
+          default: ''
+        }
+      ],
+      guildOnly: true,
+    });
+  }
+
+  async run(msg, args) {
+    // Just output the prefix
+    if (!args.prefix) {
+      const prefix = msg.guild ? msg.guild.commandPrefix : this.client.commandPrefix;
+      return msg.reply(stripIndents`
+				${prefix ? `The command prefix is \`\`${prefix}\`\`.` : 'There is no command prefix.'}
+				To run commands, use ${msg.anyUsage('command')}.
+			`);
+    }
+
+    // Check the user's permission before changing anything
+    if (msg.guild) {
+      if (!msg.member.hasPermission('ADMINISTRATOR') && !this.client.isOwner(msg.author)) {
+        return msg.reply('Only administrators may change the command prefix.');
+      }
+    } else if (!this.client.isOwner(msg.author)) {
+      return msg.reply('Only the bot owner(s) may change the global command prefix.');
+    }
+
+    // Save the prefix
+    const lowercase = args.prefix.toLowerCase();
+    const prefix = lowercase === 'none' ? '' : args.prefix;
+    let response;
+    if (lowercase === 'default') {
+      if (msg.guild) msg.guild.commandPrefix = null; else this.client.commandPrefix = null;
+      const current = this.client.commandPrefix ? `\`\`${this.client.commandPrefix}\`\`` : 'no prefix';
+      response = `Reset the command prefix to the default (currently ${current}).`;
+      msg.channel.send(response);
+    } else {
+      let oldPrefix = await writePrefix(prefix, msg)
+      if (msg.guild) msg.guild.commandPrefix = prefix; else this.client.commandPrefix = prefix;
+      const embed = new Discord.MessageEmbed()
+        .setColor('#ff548e')
+        .setDescription(`This guild prefix has been updated`)
+        .addField('From', `**${oldPrefix}**`, true)
+        .addField('To', `**${args.prefix}**`, true)
+        .addField('Usage', `${msg.anyUsage('command')}`);
+      // send embed
+      msg.channel.send(embed);
+    }
+
+
+    return null;
+  }
+};
