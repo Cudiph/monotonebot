a288b3d2a17cb91c55be27827500f1c2bc3840e0
Cudiph <dwiaceromo@gmail.com>
2020-10-02 11:28:40 +0700

    Add leave guild command

---
+++ src/Modules/administration/leaveme.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class pingCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'leaveme',
+      group: 'administration',
+      memberName: 'leaveme',
+      description: 'Why did u leave me?!',
+      examples: ['leaveme'],
+      clientPermissions: ['KICK_MEMBERS', 'ADMINISTRATOR'],
+    });
+  }
+
+  run(msg) {
+    return msg.say("`Bye`").then(msg => msg.guild.leave());
+  }
+
+  onBlock(msg, reason, data) {
+    super.onBlock(msg, reason, data).then(parent => parent.delete({ timeout: 9000 }));
+  }
+};
