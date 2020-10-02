196bffe1d98261ad6c741f04e926493ea12bd926
Cudiph <dwiaceromo@gmail.com>
2020-10-02 22:35:26 +0700

    remodel unban.js

--- src/Modules/administration/unban.js
+++ src/Modules/administration/unban.js
@@ -1,34 +1,58 @@
-const { getUserIdMention } = require('../../library/users/get-cache.js');
-module.exports = {
-  name: 'unban',
-  description: 'Unban a user from the server.',
-  usage: '<user>',
-  args: true,
-  guildOnly: true,
-  execute(message, args) {
-    if (!message.member.hasPermission("BAN_MEMBERS")) {
-      return message.reply(`You do not have permission to do that!`);
+const { getUserIdMention, isUserId } = require('../../library/users/get-cache.js')
+const { Command } = require('discord.js-commando')
+
+module.exports = class HelpCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'unban',
+      group: 'administration',
+      memberName: 'unban',
+      description: 'Unban someone with mentioning or by id',
+      examples: ['unban @someone', 'unban @someone Your reason here','unban 623491289324'],
+      guarded: true,
+      guildOnly: true,
+      argsType: 'multiple',
+      clientPermissions: ['BAN_MEMBERS'],
+      userPermissions: ['BAN_MEMBERS']
+    });
+  }
+
+  async run(msg, args) {
+    let bannedUser = await msg.guild.fetchBan(args[0]);
+    let isMember = args[0].match(/^<@!?\d+>$/);
+    // check whether the id is valid
+    if (bannedUser) {
+      bannedUser = args[0];
+    } else if (isMember) {
+      bannedUser = await getUserIdMention(args[0], msg);
+    } else {
+      return msg.say('Invalid Id or Argument');
     }
 
-    if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
-      return message.channel.send(`**${message.author.username}**, I do not have permission to unban someone`);
-    }
-
-    let id = getUserIdMention(args[0], message);
-    if (id) {
-      message.guild.members.unban(id, args.slice(1).join(' '))
-        .then(user => message.channel.send(`Unbanned **${user.username}#${user.discriminator}** from **${message.guild.name}**`))
+    if (bannedUser) {
+      msg.guild.members.unban(bannedUser, args.slice(1).join(' '))
+        .then(user => msg.channel.send(`Unbanned **${user.username}#${user.discriminator}** from **${msg.guild.name}**`))
         .catch(err => {
           // due to missing permissions or role hierarchy
-          message.reply('I was unable to unban the member\n' +
+          msg.reply('I was unable to unban the member\n' +
             '**Error name** : ' + err.toString().split(':').slice(1).join());
           // Log the error
           logger.log('error', err);
         });
 
     } else {
-      message.reply(`You didn't mention user to unban`);
+      msg.reply(`You didn't mention user to unban`);
     }
 
   }
-}
+
+  async onBlock(msg, reason, data) {
+    let parent = await super.onBlock(msg, reason, data);
+    parent.delete({ timeout: 9000 })
+  }
+
+  onError(err, message, args, fromPattern, result) {
+    super.onError(err, message, args, fromPattern, result)
+      .then(msgParent => msgParent.delete({ timeout: 9000 }));
+  }
+};
