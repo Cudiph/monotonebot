0014b20df9d1cf1e35fe598cac7d04397db09ffa
Cudiph <dwiaceromo@gmail.com>
2020-10-02 11:27:51 +0700

    Fixing bug when db is always fetched

--- src/events/message.js
+++ src/events/message.js
@@ -4,36 +4,27 @@ const { guildSettingsSchema } = require('../library/Database/schema.js');
 
 // event on message
 client.on('message', async message => {
-  // const skrg = Date.now();
-  // fetch prefix from database
-  if(message.guild) {
+    // fetch prefix from database
+  if (message.guild) {
     if (!message.guild.isCached) {
       // fetch prefix from database
-      await new crud(process.env.MONGO_URL).connect();
+      let db = await new crud(process.env.MONGO_URL);
+      db.connect();
       try {
-        const check = await guildSettingsSchema.findById(message.guild.id);
+        const check = await db.findById(guildSettingsSchema, message.guild.id);
         if (!check) {
-          // if data from database is null then create default prefix
-          const result = await guildSettingsSchema.findOneAndUpdate({
-            _id: message.guild.id
-          }, {
+          let result = db.writeOneUpdate(guildSettingsSchema, { _id: message.guild.id }, {
             _id: message.guild.id,
             prefix: client.commandPrefix,
-          },
-            {
-              upsert: true
-            });
+          });
           message.guild.commandPrefix = result.prefix;
         } else {
           message.guild.commandPrefix = check.prefix;
         }
-
       } catch (err) {
         logger.log('error', err);
       }
-    } else {
-      message.guild.isCached = true;
-      // console.log(Date.now() - skrg);
     }
+    message.guild.isCached = true;
   }
 });
