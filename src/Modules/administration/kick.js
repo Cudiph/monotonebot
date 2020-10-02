24c07ed4a2d20f86710cd8042427c77316c8a296
Cudiph <dwiaceromo@gmail.com>
2020-10-02 23:09:18 +0700

    remodel kick.js

--- src/Modules/administration/kick.js
+++ src/Modules/administration/kick.js
@@ -1,22 +1,34 @@
 const Discord = require('discord.js');
-const { getUserMention } = require('../../library/users/get-cache.js')
+const { getUserMention, isUserId } = require('../../library/users/get-cache.js')
+const { Command } = require('discord.js-commando')
 
-module.exports = {
-  name: 'kick',
-  description: 'Kick a user from the server.',
-  usage: '<user>',
-  args: true,
-  guildOnly: true,
-  execute(message, args) {
-    if (!message.member.hasPermission("KICK_MEMBERS")) {
-      return message.reply(`You do not have permission to do that!`);
+module.exports = class KickCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'kick',
+      group: 'administration',
+      memberName: 'kick',
+      description: 'Kick someone with mentioning or by id',
+      examples: ['kick @someone', 'kick @someone Bad words', 'kick 71283497224353324 Your reason here'],
+      guarded: true,
+      guildOnly: true,
+      argsType: 'multiple',
+      clientPermissions: ['KICK_MEMBERS'],
+      userPermissions: ['KICK_MEMBERS']
+    });
+  }
+
+  async run(msg, args) {
+    let member = await isUserId(args[0], msg);
+    let isMember = args[0].match(/^<@!?\d+>$/);
+    // check if the member is exist
+    if (member) {
+      member = await msg.guild.members.cache.get(args[0]);
+    } else if (isMember) {
+      member = await getUserMention(args[0], msg);
+    } else {
+      return msg.say('Invalid Id or Argument');
     }
-
-    if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
-      return message.channel.send(`**${message.author.username}**, I do not have permission to kick !`);
-    }
-
-    const member = getUserMention(args[0], message);
     const kickedName = `${member.user.username}#${member.user.discriminator} <${member.user.id}>`;
     const kickedimage = `${member.user.displayAvatarURL()}`;
     if (member) {
@@ -29,24 +41,33 @@ module.exports = {
             .setAuthor(kickedName, kickedimage)
             .setTitle(`Kicked Successfully`)
             .setDescription(`**Member** : ${kickedName}\n` + `**Reason** : ${args.slice(1).join(' ') || '-'}\n` +
-              `**Time** : ${message.createdAt.toUTCString()}`)
-            .setFooter(`Kicked by ${message.author.username}#${message.author.discriminator}`,
-              `${message.author.displayAvatarURL()}`);
+              `**Time** : ${msg.createdAt.toUTCString()}`)
+            .setFooter(`Kicked by ${msg.author.username}#${msg.author.discriminator}`,
+              `${msg.author.displayAvatarURL()}`);
 
-          message.channel.send({
+          msg.channel.send({
             embed: EmbedMsg
           });
         })
         .catch(err => {
           // due to missing permissions or role hierarchy
-          message.reply('I was unable to kick the member');
+          msg.reply('I was unable to kick the member');
           // Log the error
           logger.log('error', err);
         });
     }
-    // Otherwise, if no user was mentioned
     else {
-      message.reply("You didn't mention the user to kick!");
+      return msg.reply("You didn't mention the user to kick!");
     }
   }
-}
+
+  async onBlock(msg, reason, data) {
+    let parent = await super.onBlock(msg, reason, data);
+    parent.delete({ timeout: 9000 })
+  }
+
+  onError(err, message, args, fromPattern, result) {
+    super.onError(err, message, args, fromPattern, result)
+      .then(msgParent => msgParent.delete({ timeout: 9000 }));
+  }
+};
