538c36333f7e519b462c8af0cb85369a15122af2
Cudiph <dwiaceromo@gmail.com>
2020-10-02 11:26:44 +0700

    add guildDelete event

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -4,21 +4,30 @@ const { guildSettingsSchema } = require('../library/Database/schema.js');
 
 // event on message
 client.on('guildCreate', async guild => {
-   let db = await new crud(process.env.MONGO_URL).connect();
+  let db = await new crud(process.env.MONGO_URL);
+  db.connect();
   try {
-    await guildSettingsSchema.findOneAndUpdate({
-      _id: guild.id
-    },{
+    await db.writeOneUpdate(guildSettingsSchema, { _id: guild.id }, {
       _id: guild.id,
       prefix: '..'
-    },
-    {
-      upsert: true
     });
   } catch (err) {
     logger.log('error', err);
   } finally {
-    db.connection.close();
+    db.close();
   }
 
 });
+
+client.on('guildDelete', async guild => {
+  let db = await new crud(process.env.MONGO_URL);
+  db.connect();
+  try {
+    await db.findByIdDelete(guildSettingsSchema, guild.id);
+  } catch (err) {
+    logger.log(err);
+  } finally {
+    db.close();
+  }
+
+})
