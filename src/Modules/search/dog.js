01dec1153f019d0f06a37370943f4d694815e8ef
Cudiph <dwiaceromo@gmail.com>
2020-10-03 23:07:23 +0700

    remodel dog.js

--- src/Modules/search/dog.js
+++ src/Modules/search/dog.js
@@ -1,13 +1,34 @@
 const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
 
-module.exports = {
-  name: 'dog',
-  cooldown: 10,
-  description: 'Send a random dog image',
-  async execute(message) {
+module.exports = class DogCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'dog',
+      group: 'search',
+      memberName: 'dog',
+      description: 'Send a random dog image',
+      examples: ['dog'],
+      throttling: {
+        usages: 5,
+        duration: 10,
+      },
+    })
+  }
+
+  async run(msg) {
     // get the data
     const response = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
-    message.channel.send(response.message);
+    msg.channel.send(response.message);
+  }
 
+  async onBlock(msg, reason, data) {
+    let parent = await super.onBlock(msg, reason, data);
+    parent.delete({ timeout: 9000 })
+  }
+
+  onError(err, message, args, fromPattern, result) {
+    super.onError(err, message, args, fromPattern, result)
+      .then(msgParent => msgParent.delete({ timeout: 9000 }));
   }
 }
