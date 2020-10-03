0061a383a263a639c10abbeedb5f5e7ef20c1e6d
Cudiph <dwiaceromo@gmail.com>
2020-10-03 23:07:06 +0700

    remodel cat.js

--- src/Modules/search/cat.js
+++ src/Modules/search/cat.js
@@ -1,12 +1,35 @@
 const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
 
-module.exports = {
-  name: 'cat',
-  cooldown: 10,
-  description: 'Send a random cat image',
-  async execute(message) {
+module.exports = class CatCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'cat',
+      group: 'search',
+      aliases: ['meow'],
+      memberName: 'cat',
+      description: 'Send a random cat image',
+      examples: ['cat'],
+      throttling: {
+        usages: 5,
+        duration: 10,
+      },
+    })
+  }
+
+  async run(msg) {
     // get the data
     const response = await fetch('https://aws.random.cat/meow').then(response => response.json());
-    message.channel.send(response.file);
+    msg.channel.send(response.file);
+  }
+
+  async onBlock(msg, reason, data) {
+    let parent = await super.onBlock(msg, reason, data);
+    parent.delete({ timeout: 9000 })
+  }
+
+  onError(err, message, args, fromPattern, result) {
+    super.onError(err, message, args, fromPattern, result)
+      .then(msgParent => msgParent.delete({ timeout: 9000 }));
   }
 }
