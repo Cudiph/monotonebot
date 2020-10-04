dc3c88949bd8c1f5ba07d82e9b77f742a8b5084d
Cudiph <dwiaceromo@gmail.com>
2020-10-04 23:46:54 +0700

    Some major optimization to play.js

--- src/Modules/voice/play.js
+++ src/Modules/voice/play.js
@@ -1,103 +1,9 @@
 const ytdl = require('ytdl-core-discord');
 const yts = require('yt-search');
-const Discord = require('discord.js');
-const emoji = require('../../library/helper/emoji.js');
+const { emoji } = require('../../library/helper/discord-item.js');
 const { Command } = require('discord.js-commando');
 const { oneLine } = require('common-tags');
-
-
-async function play(message) {
-  let queue = message.guild.queue;
-
-  // if queue is null then delete the object property in cache variable
-  if (!queue) {
-    message.channel.send(`Stopped Playing...`);
-    return delete guildQueue[guildId];
-  }
-
-  const connection = await message.member.voice.channel.join();
-  // make a stream dispatcher
-  let dispatcher = await connection.play(await ytdl(guildQueue[guildId][0].link), { type: 'opus', filter: 'audioonly', volume: 0.5 });
-
-  guildQueue[guildId][0].dispatcher = dispatcher;
-
-  connection.on('disconnect', () => {
-    delete guildQueue[guildId];
-  })
-
-  dispatcher.on('start', () => {
-    message.channel.send('now playing\n' + guildQueue[guildId][0].title + ` by ${guildQueue[guildId][0].uploader}`);
-  });
-
-  dispatcher.on('finish', () => {
-    guildQueue[guildId].shift();
-    play(message)
-  });
-
-  dispatcher.on('error', err => logger.log('error', err));
-}
-async function player(data, message) {
-  const guildId = message.guild.id;
-  if (!guildQueue[guildId]) {
-    try {
-      const construction = {
-        title: data.title,
-        link: data.uri,
-        uploader: data.author.name,
-        channel: message.channel
-      }
-      guildQueue[guildId] = [];
-      await guildQueue[guildId].push(construction);
-      return play(message);
-    } catch (err) {
-      delete guildQueue[guildId];
-      logger.log('error', err);
-    }
-  } else {
-    const construction = {
-      title: data.title,
-      link: data.uri,
-      uploader: data.author.name,
-      channel: message.channel
-    }
-    guildQueue[guildId].push(construction);
-    return message.channel.send(`${data.title} has been added to the queue.`)
-  }
-}
-
-function setEmbed(dataList, indexPage, page, msg, itemsPerPage) {
-  let listLength = dataList.length;
-  let embed = {
-    color: 0x53bcfc,
-    author: {
-      name: `@${msg.author.username}#${msg.author.discriminator}`,
-      icon_url: msg.author.displayAvatarURL(),
-    },
-    description: `React with emoji to select audio`,
-    fields: [],
-    footer: {
-      text: `${page + 1}/${Math.ceil(listLength / itemsPerPage)}`,
-    },
-  }
-
-  if ((page + 1) === Math.ceil(listLength / itemsPerPage)) {
-    for (let i = 0; i < (listLength - indexPage); i++) {
-      embed.fields.push({
-        name: `[${i + 1}] ${dataList[indexPage + i].title}`,
-        value: `Uploaded by ${dataList[indexPage + i].author.name} | ${dataList[indexPage + i].timestamp}`,
-      })
-    }
-  } else {
-    for (let i = 0; i < itemsPerPage; i++) {
-      embed.fields.push({
-        name: `[${i + 1}] ${dataList[indexPage + i].title}`,
-        value: `Uploaded by ${dataList[indexPage + i].author.name} | ${dataList[indexPage + i].timestamp}`,
-      })
-    }
-  }
-
-  return embed;
-}
+const { setEmbedPlayCmd, player } = require('../../library/helper/player.js');
 
 module.exports = class PlayCommand extends Command {
   constructor(client) {
@@ -129,41 +35,36 @@ module.exports = class PlayCommand extends Command {
     }
 
     if (message.member.voice.channel) {
-      const link = args[0].match(/https?:\/\/www.youtube.com\/watch\?v=\w+/)
+      const link = args[0].match(/https?:\/\/(www.)?youtube.com\/watch\?v=\w+/)
 
       // check if author send a youtube link
       if (link) {
-        let data = ytdl.getInfo(args[0])
+        let data = await ytdl.getInfo(args[0]);
         let dataConstructor = {
           title: data.title,
-          uri: args[0],
-          author: { name: data.author },
+          url: data.url,
+          author: data.author,
+          seconds: data.videoDetails.lengthSeconds,
           channel: message.channel
         }
         return player(dataConstructor, message);
       }
 
-      let { videos } = await yts(args.join(' '))
+      message.channel.startTyping(); // start type indicator cuz it'll be long
+      let { videos } = await yts(args.join(' ')) // fetch yt vid using yt-search module
 
       let page = 0; // for page
       let music = 0; // for choosing music index
       let itemsPerPage = 5; // set items showed per page
 
-      let Embed = setEmbed(videos, music, page, message, itemsPerPage);
+      let Embed = setEmbedPlayCmd(videos, music, page, message, itemsPerPage);
+      let emojiNeeded = [emoji[1], emoji[2], emoji[3], emoji[4], emoji[5], emoji.leftA, emoji.rightA, emoji.x];
 
-      message.channel.send({embed: Embed}).then(async msg => {
-        msg.react(emoji[1]);
-        msg.react(emoji[2]);
-        msg.react(emoji[3]);
-        msg.react(emoji[4]);
-        msg.react(emoji[5]);
-        msg.react(emoji.leftA);
-        msg.react(emoji.rightA);
-        msg.react(emoji.x);
+      message.channel.send({ embed: Embed }).then(async msg => {
+        msg.channel.stopTyping(true); // stop typing indicator
 
         const filter = (reaction, user) => {
-          return [emoji[1], emoji[2], emoji[3], emoji[4], emoji[5], emoji.leftA, emoji.rightA, emoji.x]
-            .includes(reaction.emoji.name) && user.id === message.author.id;
+          return emojiNeeded.includes(reaction.emoji.name) && user.id === message.author.id;
         };
         const collector = msg.createReactionCollector(filter, { time: 60000, dispose: true });
 
@@ -191,44 +92,25 @@ module.exports = class PlayCommand extends Command {
             }
           }
           if (collected.emoji.name === '➡' || collected.emoji.name === '⬅') {
-            let embed2 = setEmbed(videos, music, page, message, itemsPerPage);
-
-            return msg.edit({embed: embed2});
+            let embed2 = setEmbedPlayCmd(videos, music, page, message, itemsPerPage);
+            return msg.edit({ embed: embed2 });
           }
 
-          if (collected.emoji.name === emoji[1]) {
-            let data = videos[music];
-            msg.edit(`${data.title} has been added to the queue.`).then(msg => {
-              msg.delete();
-            })
-            return player(data, message);
-          } else if (collected.emoji.name === emoji[2]) {
-            let data = videos[music + 1];
-            msg.edit(`${data.title} has been added to the queue.`).then(msg => {
-              msg.delete();
-            })
-            return player(data, message);
-          } else if (collected.emoji.name === emoji[3]) {
-            let data = videos[music + 2];
-            msg.edit(`${data.title} has been added to the queue.`).then(msg => {
-              msg.delete();
-            })
-            return player(data, message);
-          } else if (collected.emoji.name === emoji[4]) {
-            let data = videos[music + 3];
-            msg.edit(`${data.title} has been added to the queue.`).then(msg => {
-              msg.delete();
-            })
-            return player(data, message);
-          } else if (collected.emoji.name === emoji[5]) {
-            let data = videos[music + 4];
-            msg.edit(`${data.title} has been added to the queue.`).then(msg => {
-              msg.delete();
-            })
+          if (emojiNeeded.slice(0, 5).includes(collected.emoji.name)) {
+            let reversed = Object.keys(emoji).find(key => emoji[key] === collected.emoji.name);
+            let intEmoji = parseInt(reversed);
+            let data = videos[music + intEmoji - 1];
+            msg.delete();
             return player(data, message);
           }
         })
-      })
+
+        for (let i = 0; i < emojiNeeded.length; i++) {
+          if (msg) {
+            await msg.react(emojiNeeded[i]);
+          }
+        }
+      }).catch(err => null); // cuz error is normal in this case
 
     }
 
