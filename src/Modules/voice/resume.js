1f12a1b726822a9d09eeb420d5c99736afab283e
Cudiph <dwiaceromo@gmail.com>
2020-10-05 00:46:04 +0700

    Add new command resume.js

---
+++ src/Modules/voice/resume.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class ResumeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'resume',
+      group: 'voice',
+      memberName: 'resume',
+      description: 'Pause a stream',
+      examples: ['resume'],
+      guildOnly: true,
+      throttling: {
+        usages: 3,
+        duration: 10,
+      },
+    })
+  }
+
+  async run(msg) {
+    if (!msg.guild.me.voice.connection) {
+      return msg.say(`I'm not connected to the voice channel`);
+    }
+    if (msg.guild.me.voice.connection.dispatcher) {
+      return msg.guild.me.voice.connection.dispatcher.resume();
+    }
+  }
+
+  async onBlock(msg, reason, data) {
+    let parent = await super.onBlock(msg, reason, data);
+    parent.delete({ timeout: 9000 });
+  }
+
+  onError(err, message, args, fromPattern, result) {
+    super.onError(err, message, args, fromPattern, result)
+      .then(msgParent => msgParent.delete({ timeout: 9000 }));
+  }
+}
