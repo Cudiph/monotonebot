864fb64580b98740c9536e1728f39c50e463b6c4
Cudiph <dwiaceromo@gmail.com>
2020-10-10 14:11:04 +0700

    Add new command status.js to change bot status

---
+++ src/Modules/administration/status.js
@@ -0,0 +1,44 @@
+const { Command } = require('discord.js-commando')
+
+module.exports = class SetUsernameCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'setstatus',
+      group: 'administration',
+      memberName: 'status',
+      description: 'Set bot username',
+      examples: ['setstatus idle'],
+      details: 'available status are online, idle, invisible, dnd(do not disturb)',
+      guarded: true,
+      ownerOnly: true,
+      argsType: 'multiple',
+      throttling: {
+        usages: 2,
+        duration: 3600,
+      },
+    });
+  }
+
+  async run(msg, args) {
+    // Set username
+    msg.client.user.setStatus(args[0])
+      .then(msg.say(`Change status to \`${args[0]}\``))
+      .catch(err => {
+        msg.say('Something went wrong');
+        logger.log('error', err);
+      });
+  }
+
+  async onBlock(msg, reason, data) {
+    let parent = await super.onBlock(msg, reason, data);
+    parent.delete({ timeout: 9000 })
+  }
+
+  onError(err, message, args, fromPattern, result) {
+    super.onError(err, message, args, fromPattern, result)
+      .then(msgParent => msgParent.delete({ timeout: 9000 }));
+  }
+};
+
+
+
