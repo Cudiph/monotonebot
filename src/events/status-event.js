d149d14835ad0858925b1bb025883d1d81df642d
Cudiph <dwiaceromo@gmail.com>
2020-10-10 14:08:23 +0700

    Dispaly activity when bootup

--- src/events/status-event.js
+++ src/events/status-event.js
@@ -2,6 +2,7 @@ const { client } = require('../bot.js');
 
 // logging to the console if the bot is ready
 client.once('ready', () => {
+  client.user.setActivity('..help Command', { type: 'PLAYING' })
   logger.log('info', 'Ready!');
 });
 
