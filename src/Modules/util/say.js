8fc4e6e9bc37f8fab329705d60b475f92315ad30
Cudiph <dwiaceromo@gmail.com>
2020-11-29 02:08:32 +0700

    Add commando args in say.js

--- src/Modules/util/say.js
+++ src/Modules/util/say.js
@@ -15,29 +15,42 @@ module.exports = class SayCommand extends Command {
         duration: 5,
       },
       examples: ['say Hello Dear', 'say #announcement Announcement \n\n I\'m live on twitch'],
+      args: [
+        {
+          key: 'textChannel',
+          prompt: 'Which text channel to send? (optional) type first word to continue',
+          type: 'text-channel|string',
+        },
+        {
+          key: 'words',
+          prompt: 'What do you want to say?',
+          type: 'string',
+        }
+      ],
     });
   }
 
-  async run(msg, args) {
+  async run(msg, {textChannel, words}) {
     let channel;
-    let isChannel = args[0].match(/^<#\d+>$/);
-    if (isChannel) {
-      channel = getChannelMention(args[0], msg);
+    if (typeof textChannel == 'object') {
+      channel = textChannel
     }
 
     // embeded msg
     let embedMsg = {
-      description: args.slice(1).join(' '),
+      description: words,
       color: 0xff548e,
     }
 
+    // check if user mention a channel if not send all args in that channel
     if (!channel) {
       let embedMsg = new Discord.MessageEmbed()
         .setColor('#ff548e')
-        .setDescription(args.join(' '));
+        .setDescription(textChannel.concat(' ', words));
       return msg.channel.send(embedMsg);
     }
 
+    // check if the user have perm to send the message
     if (!channel.permissionsFor(msg.author.id).has('SEND_MESSAGES')) {
       return msg.channel.send(`You don't have a permission for sending messages to that channel`);
     }
