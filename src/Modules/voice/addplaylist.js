ffd1ed2f92807de8d89617f8fbedb1df1375772c
Cudiph <dwiaceromo@gmail.com>
2020-11-30 09:41:34 +0700

    Add commando args and improve playlist filtering in playlist.js

--- src/Modules/voice/addplaylist.js
+++ src/Modules/voice/addplaylist.js
@@ -13,7 +13,6 @@ module.exports = class AddPlaylistCommand extends Command {
       description: 'Take a list from youtube an push all vids to the queue',
       examples: ['addplaylist PLtByeX9ycvplxbyVX7fiUA4f_OxiyNOxk',
         'addplaylist https://www.youtube.com/watch?v=q6EoRBvdVPQ&list=PLFsQleAWXsj_4yDeebiIADdH5FMayBiJo'],
-      argsType: 'multiple',
       guildOnly: true,
       details: oneLine`
       Take a playlist from youtube and add all of the video to
@@ -25,33 +24,36 @@ module.exports = class AddPlaylistCommand extends Command {
         duration: 30,
       },
       clientPermissions: ['CONNECT', 'SPEAK'],
+      format: '["Video Id"/"Full url"]',
+      args: [
+        {
+          key: 'listId',
+          prompt: 'What playlist you want to add? (list id or full url)',
+          type: 'string',
+        }
+      ]
     })
   }
 
-  async run(msg, args) {
+  async run(msg, { listId }) {
     // if not in voice channel
     if (!msg.member.voice.channel) {
       // send msg if author not connected to voice channel
       return msg.channel.send("You're not connected to any voice channel");
     }
-    if (!args.length) {
-      return msg.say('Please provide an argument');
-    }
-    if (!msg.guild.me.voice.connection) {
-      msg.member.voice.channel.join();
-    }
 
     if (msg.member.voice.channel) {
-      let link = args[0].match(/https?:\/\/(www.)?youtube.com\/watch\?(.*)?list=(\w+)(.*)?/);
+      let link = listId.match(/(?:.*)?list=([\w+|-]+)(?:.*)?/);
       if (link) {
         try {
-          let playlist = await yts({ listId: link[3] });
+          let playlist = await yts({ listId: link[1] });
           // list of videos in the playlist
           const videos = playlist.videos
           videos.forEach(async video => {
             await player({
               title: video.title,
               url: `https://youtube.com/watch?v=${video.videoId}`,
+              videoId: videos.videoId,
               author: video.author,
               seconds: 0,
             }, msg, true);
@@ -59,16 +61,20 @@ module.exports = class AddPlaylistCommand extends Command {
           return msg.say(`Added playlist ${playlist.title} by ${playlist.author.name}`);
         } catch (err) {
           logger.log('error', err)
-          return msg.say('An error Occured');
+          return msg.say(oneLine`An error Occured, \n
+            Maybe it's because the playlist is private or the playlist is from a mix or the playlist doesn't exist at all \n
+            Error : \`${err}\`
+          `);
         }
       } else {
         try {
-          let playlist = await yts({ listId: args[0] });
+          let playlist = await yts({ listId: listId });
           const videos = playlist.videos
           videos.forEach(async video => {
             await player({
               title: video.title,
               url: `https://youtube.com/watch?v=${video.videoId}`,
+              videoId: videos.videoId,
               author: video.author,
               seconds: 0,
             }, msg, true);
@@ -76,7 +82,10 @@ module.exports = class AddPlaylistCommand extends Command {
           return msg.say(`Added playlist ${playlist.title} by ${playlist.author.name}`);
         } catch (err) {
           logger.log('error', err)
-          return msg.say('An error Occured');
+          return msg.say(oneLine`An error Occured, \n
+            Maybe it's because the playlist is private or the playlist is from a mix or the playlist doesn't exist at all \n
+            Error : \`${err}\`
+          `);
         }
       }
     }
