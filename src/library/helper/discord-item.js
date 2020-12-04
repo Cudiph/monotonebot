759c776e6cb045481ed261c80cb80e91866dcf7e
Cudiph <dwiaceromo@gmail.com>
2020-12-05 03:24:14 +0700

    Add new method toSeconds to convert timestamp

--- src/library/helper/discord-item.js
+++ src/library/helper/discord-item.js
@@ -21,16 +21,45 @@ function randomHex() {
   return Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0');
 }
 
-function toTimestamp(second) {
-  if (second > 3599) {
-    return new Date(second * 1000).toISOString().substr(11, 8);
+/**
+ * Change seconds to timestamp like '04:20:46'
+ * @param {Number} seconds seconds in numbers
+ */
+function toTimestamp(seconds) {
+  let day = Math.floor(seconds / 86400);
+  if (seconds > 86400) {
+    return day.toString() + ':' + new Date(seconds * 1000).toISOString().substr(11, 8);
+  } else if (seconds > 3599) {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
   } else {
-    return new Date(second * 1000).toISOString().substr(14, 5);
+    return new Date(seconds * 1000).toISOString().substr(14, 5);
   }
+}
+
+/**
+ * change timestamps to seconds format like 3600
+ * @param {String} timestamp String from timestamp like 10:00:00
+ */
+function toSeconds(timestamp) {
+  // reverse the splitted timestamp from 10:00:00 to ['00','00','10']
+  // so it start from secs, mins, hours
+  let time = timestamp.split(/\s*:\s*/).reverse();
+  let seconds = 0;
+  for (let i = 0; i < time.length; i++) {
+    if (i === 0) {
+      seconds += parseInt(time[i]);
+    } else if (i <= 3) {
+      seconds += parseInt(time[i]) * 60 ** i;
+    } else {
+      seconds += parseInt(time[i]) * 86400;
+    }
+  }
+  return seconds;
 }
 
 module.exports = {
   emoji,
   randomHex,
   toTimestamp,
+  toSeconds,
 };
