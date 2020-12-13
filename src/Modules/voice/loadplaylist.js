e31b4857795b92905e190d5637ed175d22bc4289
Cudiph <dwiaceromo@gmail.com>
2020-12-13 17:55:21 +0700

    Load playlist now can search with the name of the playlist

--- src/Modules/voice/loadplaylist.js
+++ src/Modules/voice/loadplaylist.js
@@ -1,5 +1,5 @@
 const { oneLine } = require('common-tags');
-const { Command } = require('discord.js-commando');
+const { Command, CommandoMessage } = require('discord.js-commando');
 const { userDataSchema } = require('../../library/Database/schema.js');
 const { play } = require('../../library/helper/player.js');
 
@@ -12,6 +12,11 @@ module.exports = class LoadPlaylistCommand extends Command {
       examples: ['loadpl 1', 'loadplaylist 3'],
       memberName: 'loadplaylist',
       description: 'Load playlist from database',
+      details: oneLine`
+        Basic regular expression such as asterisk or caret are allowed.
+        If client received 2 or more playlists, it'll take a random playlists
+        and then pushed to the guild queue.
+      `,
       guildOnly: true,
       throttling: {
         usages: 3,
@@ -19,15 +24,17 @@ module.exports = class LoadPlaylistCommand extends Command {
       },
       args: [
         {
-          key: 'playlistId',
-          prompt: 'What\'s the id of your playlist?',
-          type: 'integer',
+          key: 'playlistArg',
+          label: 'PlaylistName|PlaylistId',
+          prompt: 'What\'s the name or id of your playlist?',
+          type: 'integer|string',
         }
       ]
     })
   }
 
-  async run(msg, { playlistId }) {
+  /** @param {CommandoMessage} msg */
+  async run(msg, { playlistArg }) {
     if (!msg.member.voice.channel) {
       return msg.channel.send("You're not connected to any voice channel");
     }
@@ -38,29 +45,73 @@ module.exports = class LoadPlaylistCommand extends Command {
       `);
     }
 
-    try {
-      const data = await userDataSchema.findOne({ userId: msg.author.id });
-      if (!data || !data.userPlaylists.length) {
-        return msg.say('You don\'t have any playlist');
-      } else if (playlistId < 0 && playlistId > data.userPlaylists.length) {
-        return msg.say(`Your current playlist is from 0-${data.userPlaylists.length - 1}`);
-      }
-      const playlist = data.userPlaylists[playlistId];
-      if (!msg.guild.queue) {
-        msg.guild.queue = playlist.videoList;
-        msg.guild.indexQueue = 0;
-        play(msg);
+    if (typeof playlistArg === 'number') {
+      try {
+        const data = await userDataSchema.findOne({ userId: msg.author.id });
+        if (!data || !data.userPlaylists.length) {
+          return msg.say('You don\'t have any playlist');
+        } else if (playlistArg < 0 && playlistArg > data.userPlaylists.length) {
+          return msg.say(`Your current playlist range is from 0-${data.userPlaylists.length - 1}`);
+        }
+        const playlist = data.userPlaylists[playlistArg];
+        if (!msg.guild.queue) {
+          msg.guild.queue = playlist.videoList;
+          msg.guild.indexQueue = 0;
+          play(msg);
+          return msg.say(`Added playlist **${playlist.name}**.`);
+        }
+        const oldLength = msg.guild.queue.length;
+        msg.guild.queue.push(...playlist.videoList);
+        if (msg.guild.indexQueue >= oldLength) {
+          play(msg);
+        }
         return msg.say(`Added playlist **${playlist.name}**.`);
+      } catch (err) {
+        logger.log('error', err);
+        return msg.reply(`Can't load the playlist`);
       }
-      const oldLength = msg.guild.queue.length;
-      msg.guild.queue.push(...playlist.videoList);
-      if (msg.guild.indexQueue >= oldLength) {
-        play(msg);
+    } else {
+      const secureStr = playlistArg.replace(/(\\|\(|\{|\$)/gi, '');
+      try {
+        const data = await userDataSchema.aggregate([
+          { $match: { userId: msg.author.id } },
+          {
+            $project: {
+              userPlaylists: {
+                $filter: {
+                  input: "$userPlaylists",
+                  as: "playlist",
+                  cond: { $regexFind: { input: "$$playlist.name", regex: new RegExp(`${secureStr}`) } }
+                }
+              }
+            }
+          },
+          { $unwind: "$userPlaylists" }
+        ])
+        if (!data.length) {
+          return msg.reply(`Can't find the specified playlist`);
+        }
+        const playlist = data[Math.floor(Math.random() * data.length)].userPlaylists;
+        if (!msg.guild.queue) {
+          msg.guild.queue = playlist.videoList;
+          msg.guild.indexQueue = 0;
+          play(msg);
+          return msg.say(`Added playlist **${playlist.name}**.`);
+        }
+        const oldLength = msg.guild.queue.length;
+        msg.guild.queue.push(...playlist.videoList);
+        if (msg.guild.indexQueue >= oldLength) {
+          play(msg);
+        }
+        return msg.say(`Added playlist **${playlist.name}**.`);
+      } catch (err) {
+        logger.log('error', err);
+        if (err.message.includes('regular expression')) {
+          return msg.reply(`Please give a valid regular expression`);
+        } else {
+          return msg.reply(`Can't load the playlist`);
+        }
       }
-      return msg.say(`Added playlist **${playlist.name}**.`);
-    } catch (err) {
-      logger.log('error', err);
-      return msg.channel.send(`Can't load the playlist`);
     }
 
   }
