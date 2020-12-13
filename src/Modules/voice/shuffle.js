e5a78fb79cb67667e8796b5c8d0afc0346c12034
Cudiph <dwiaceromo@gmail.com>
2020-12-13 09:36:56 +0700

    Add new command shuffle

---
+++ src/Modules/voice/shuffle.js
@@ -0,0 +1,43 @@
+const { Command, CommandoMessage } = require('discord.js-commando');
+
+module.exports = class ShuffleCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'shuffle',
+      group: 'voice',
+      memberName: 'shuffle',
+      description: 'Randomize next playing track',
+      examples: ['shuffle', 'shuffle true'],
+      guildOnly: true,
+      throttling: {
+        usages: 1,
+        duration: 10,
+      },
+      args: [
+        {
+          key: 'turnOn',
+          type: 'boolean',
+          prompt: 'Argument must be true or false.',
+          default: ''
+        }
+      ]
+    })
+  }
+
+  /** @param {CommandoMessage} msg */
+  async run(msg, { turnOn }) {
+    if (turnOn === '') {
+      msg.guild.shuffle = !msg.guild.shuffle;
+      msg.guild.loop = msg.guild.shuffle ? false : msg.guild.loop;
+    } else {
+      msg.guild.shuffle = turnOn;
+    }
+    const embed = {
+      color: msg.guild.shuffle ? 0x11ff00 : 0xff1100,
+      description: `Set \`shuffle\` to **${msg.guild.shuffle ? 'True' : 'False'}**`
+    }
+    return msg.say({ embed });
+  }
+
+
+}
