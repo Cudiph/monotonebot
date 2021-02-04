a895798ef9919fce360366ed643578b64d104d14
Cudiph <dwiaceromo@gmail.com>
2021-02-04 16:12:09 +0700

    refactor shuffle.js

--- src/Modules/voice/shuffle.js
+++ src/Modules/voice/shuffle.js
@@ -1,3 +1,4 @@
+const { oneLine } = require('common-tags');
 const { Command } = require('discord.js-commando');
 
 module.exports = class ShuffleCommand extends Command {
@@ -6,32 +7,34 @@ module.exports = class ShuffleCommand extends Command {
       name: 'shuffle',
       group: 'voice',
       memberName: 'shuffle',
-      description: 'Randomize next playing track',
+      description: 'Shuffle current queue',
+      details: oneLine`
+        If set to true, the current queue will be randomized but original queue
+        still remain in the memory. Set to false to load the original queue.
+      `,
       examples: ['shuffle', 'shuffle true'],
       guildOnly: true,
       throttling: {
         usages: 2,
-        duration: 10,
+        duration: 30,
       },
-      args: [
-        {
-          key: 'turnOn',
-          type: 'boolean',
-          prompt: 'Argument must be true or false.',
-          default: ''
-        }
-      ]
     });
   }
 
   /** @param {import("discord.js-commando").CommandoMessage} msg */
-  async run(msg, { turnOn }) {
-    if (turnOn === '') {
-      msg.guild.shuffle = !msg.guild.shuffle;
-    } else {
-      msg.guild.shuffle = turnOn;
+  async run(msg) {
+    if (!msg.guild.queue || !msg.guild.queue.length) {
+      return msg.reply(`The queue is empty.`);
     }
-    msg.guild.loop = msg.guild.shuffle ? false : msg.guild.loop;
+    msg.guild.shuffle = !msg.guild.shuffle;
+    if (msg.guild.shuffle) {
+      msg.guild.loop = false;
+      msg.guild.queueTemp = msg.guild.queue.slice();
+      shuffleArray(msg.guild.queue);
+    } else {
+      msg.guild.queue = msg.guild.queueTemp.slice();
+      delete msg.guild.queueTemp;
+    }
     const embed = {
       color: msg.guild.shuffle ? 0x11ff00 : 0xff1100,
       description: `Set \`shuffle\` to **${msg.guild.shuffle ? 'True' : 'False'}**`
@@ -41,3 +44,16 @@ module.exports = class ShuffleCommand extends Command {
 
 
 };
+
+/**
+ * Shuffle array using Durstenfeld shuffle
+ * @see {@link https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array}
+ * @param {Array<any>} arr
+ */
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
