c6da10670f2c4f7fe39cf18c422ce1e2f9c72d2e
Cudiph <dwiaceromo@gmail.com>
2021-02-18 09:42:53 +0700

    fix: fix indexQueue calculation when indexQueue in range of removed track

--- src/Modules/voice/removeno.js
+++ src/Modules/voice/removeno.js
@@ -31,33 +31,40 @@ module.exports = class RemoveNumberCommand extends Command {
     });
   }
 
-  /** @param {import('discord.js-commando').CommandoMessage} message */
+  /** @param {import('discord.js-commando').CommandoMessage} msg */
   async run(msg, { rangeIndex }) {
-    // remove zero or more white space
-    const args = rangeIndex.split(/\s*-\s{0,}/);
+    // split from "2-5" and map to int like [2, 5]
+    const args = rangeIndex.split(/\s*-\s{0,}/).map(x => +x);
+
+    // handler
     if (!msg.guild.queue || !msg.guild.queue.length) {
-      return msg.say('There are no songs to remove');
+      return msg.reply('There are no songs to remove');
     } else if (isNaN(args[0]) || args[1] && isNaN(args[1])) {
-      return msg.say('Weird character appeared on argument').then(resMsg => resMsg.delete({ timeout: 9000 }));
+      return msg.reply('Please give a valid argument').then(resMsg => resMsg.delete({ timeout: 9000 }));
     } else if (parseInt(args[0]) > parseInt(args[1])) {
-      return msg.say('Bad calculation').then(resMsg => resMsg.delete({ timeout: 9000 }));
+      return msg.reply('Bad calculation').then(resMsg => resMsg.delete({ timeout: 9000 }));
     }
-
     if (args < 0 || args >= msg.guild.queue.length) {
-      return msg.say(`Current total queue is 0-${msg.guild.queue.length - 1}`);
+      return msg.reply(`Current total queue is 0-${msg.guild.queue.length - 1}`);
     }
 
-    let end;
-    if (!args[1]) {
-      end = 1;
-    } else {
-      end = args[1] - args[0] + 1;
-    }
+    let end; // number of track to remove
+    if (!args[1]) end = 1;
+    else end = args[1] - args[0] + 1;
+
     const removed = msg.guild.queue.splice(args[0], end);
-    if (msg.guild.indexQueue >= args[0]) {
+
+    if ((msg.guild.indexQueue >= args[0]) && (msg.guild.indexQueue < (args[0] + end))) {
+      // example condition : rmno 2-5 when indexQueue at 3 or in range 2-5
+      // then move indexQ to before the removed track so indexQ must be 1
+      msg.guild.indexQueue = args[0] - 1;
+    } else if (msg.guild.indexQueue >= args[0]) {
+      // example condition : rmno 2-5 when indexQueue at 6 or higher
       msg.guild.indexQueue -= removed.length;
     }
-    msg.say('Removed succesfully').then(resMsg => resMsg.delete({ timeout: 7000 }));
+    msg.say('Removed succesfully')
+      .then(resMsg => resMsg.delete({ timeout: 7000 }))
+      .catch(e => e);
   }
 
   async onBlock(msg, reason, data) {
