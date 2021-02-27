ca4b2170366a6bfc5f18b594c7e602cd8639121f
Cudiph <dwiaceromo@gmail.com>
2021-02-27 11:48:56 +0700

    refactor: expose bot version on bot activity

--- src/events/status-event.js
+++ src/events/status-event.js
@@ -1,9 +1,13 @@
 const { client } = require('../bot.js');
 const mongoose = require('mongoose');
+const { version } = require('../../package.json');
 
 // logging to the console if the bot is ready
 client.once('ready', () => {
-  client.user.setActivity('..help Command', { type: 'PLAYING' });
+  client.user.setActivity(
+    `@Monotone help | v${version}`,
+    { type: 'PLAYING' }
+  );
   // fetch data from database
   mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
